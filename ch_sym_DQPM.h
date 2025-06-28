6b74d87316579d91a97f164d1b0d1f7933c31941
RobPisarski <33209002+RobPisarski@users.noreply.github.com>
2025-06-28 10:10:33 -0400

    Add files via upload

---
+++ ch_sym_DQPM.h
@@ -0,0 +1,36 @@
+#include <iostream> 
+ using namespace std; 
+
+# include <cmath>
+
+
+double Pi=M_PI;
+double MPi=M_PI;
+
+double E=exp(1.0); 
+
+double Power(double x, double y)
+{
+    return pow(x,y);
+}
+
+inline double x2(double x)
+{
+    return x*x;
+}
+
+inline double x3(double x)
+{
+    return x*x*x;
+}
+
+inline double Cos(double x)
+{
+		return cos(x);
+}
+
+inline double Sin(double x)
+{
+		return sin(x);
+}
+
